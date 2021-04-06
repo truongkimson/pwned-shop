dc367a31ff6791e07b02e71819a5c6e19fbbead7
truongkimson <60567409+truongkimson@users.noreply.github.com>
2021-04-06 13:30:45 +0800

    Update AccountController.cs

--- src/pwned-shop/Controllers/AccountController.cs
+++ src/pwned-shop/Controllers/AccountController.cs
@@ -12,29 +12,15 @@ namespace pwned_shop.Controllers
     {
         public IActionResult Login()
         {
-            return View(); 
+            return View();
         }
 
         [HttpPost]
         public IActionResult Login([FromForm] LoginDetails login)
         {
             var result = PasswordHasher.CreateHash(login.Password);
-            // TODO: verify against db if credentials provided are valid and redirect to "next" page
             return Content($"Password hash is: {result[0]}\n" +
                 $"Salt is: {result[1]}");
-        }
-
-        public IActionResult Logout()
-        {
-            // TODO: Log out action, clear session, redirect to landing page
-            return Content("Not yet implemented");
-        }
-
-        public IActionResult Register()
-        {
-            // TODO: Register action, validate credentials data and persist in db
-            // redirect to account create successful page
-            return Content("Not yet implemented");
         }
     }
 }
