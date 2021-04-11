e447beb42a3501483153ebbef99061a17e128c93
truongkimson <sontks@gmail.com>
2021-04-11 15:11:19 +0800

    Amend minor erros

--- src/pwned-shop/Data/DbInitializer.cs
+++ src/pwned-shop/Data/DbInitializer.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Text.RegularExpressions;
+using System.Globalization;
 using pwned_shop.Models;
 
 namespace pwned_shop.Data
@@ -103,10 +104,9 @@ namespace pwned_shop.Data
                 {
                     Id = Convert.ToInt32(row[0]),
                     UserId = Convert.ToInt32(row[1]),
-                    Timestamp = DateTime.Parse(row[2]),
+                    Timestamp = DateTime.Parse(row[2], new CultureInfo("en-SG")),
                     PromoCode = row[3] == "" ? null : row[3]
                 };
-                Debug.WriteLine($"{o.PromoCode == null}");
                 db.Orders.Add(o);
             }
 
