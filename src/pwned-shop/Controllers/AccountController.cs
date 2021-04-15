dcd0f18663795c4072bfad8593086f64fec36f1f
truongkimson <60567409+truongkimson@users.noreply.github.com>
2021-04-15 09:17:51 +0800

    Update AccountController.cs

--- src/pwned-shop/Controllers/AccountController.cs
+++ src/pwned-shop/Controllers/AccountController.cs
@@ -103,7 +103,7 @@ namespace pwned_shop.Controllers
                 newUser.DOB = Convert.ToDateTime(user.DOB);
                 newUser.Address = user.Address;
                 db.Users.Add(newUser);
-                db.Users.Attach(newUser);
+                //db.Users.Attach(newUser);
                 db.SaveChanges();
                 return View("Success");
             }
